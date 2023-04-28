5da456bbc835502f4c0a6d133bb676fd23104af0
AdAstraGeek <adastrageek@MacBook-Pro-AdAstraGeek.local>
2023-04-28 13:58:21 -0400

    Finished protocol

--- comm.py
+++ comm.py
@@ -2,17 +2,14 @@ from crc import Calculator, Crc8
 
 import numpy as np
 
-def motor_encode(speed,left,right, calc = Calculator(Crc8.CCITT,optimized = True)):
-    prefix = 0x83
-    direction = np.array([(left << 1) | right], dtype='uint8')
-    print("Direction: " + str(direction))
-    msg = np.array(np.concatenate([direction,speed], axis=None), dtype='uint8')
-    print("Message to bytes: " + str(msg.tobytes().hex()))
+def encode(speed,left,right, calc = Calculator(Crc8.CCITT,optimized = True)):
+    msg = np.array(np.concatenate([(left << 1) | right,speed], axis=None), dtype='uint8')
     checksum = calc.checksum(msg.tobytes())
-    #0x80 last bit of the polynomial
-    msg = np.array(np.concatenate([prefix,direction+0x80,speed,checksum], axis=None), dtype='uint8').tobytes()
+    msg = np.array(np.concatenate([msg,checksum], axis=None), dtype='uint8').tobytes()
     return msg
     
+speeds = [13,71,32,64]
 
+print(encode(speeds,True,True).hex())
 
 
