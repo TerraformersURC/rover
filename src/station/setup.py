7638484491764dba92174c3548f46f230bd5ae94
sselvakumaran <sriman.selvakumaran@gmail.com>
2024-04-27 03:41:34 -0400

    reorganization

--- src/station/setup.py
+++ src/station/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'commstest'
+package_name = 'station'
 
 setup(
     name=package_name,
@@ -20,8 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "publisher = commstest.publisher:main",
-            "subscriber = commstest.subscriber:main"
+            'drivetrain_controller = station.drivetrain_controller:main'
         ],
     },
 )
