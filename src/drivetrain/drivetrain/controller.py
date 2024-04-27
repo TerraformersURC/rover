74cd80805a4f03457c84692ac8e1d11b44eb96f3
sselvakumaran <sriman.selvakumaran@gmail.com>
2024-04-27 04:13:38 -0400

    fix to controller

--- src/drivetrain/drivetrain/controller.py
+++ src/drivetrain/drivetrain/controller.py
@@ -4,13 +4,17 @@ from rclpy.node import Node
 from rclpy.timer import Timer
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64
 from interfaces.msg import MotorControl
-from interfaces.msg import OdomInfo
 from numpy import clip
 
-#the controller subscribes to /joy, /cmd_vel, /and odom_info
-#and publishes MotorControl messages to /motor_control using data from the joysticks
+DEADZONE = 0.05
+TURN_SCALING = 0.75
+LIN_AXIS = 1
+ANG_AXIS = 3
+MAX_SPEED = 1
+
+# the controller subscribes to /joy, /cmd_vel, /and odom_info
+# and publishes MotorControl messages to /motor_control using data from the joysticks
 class Controller(Node):
     _fl_speed = 0.0
     _fr_speed = 0.0
@@ -20,49 +24,39 @@ class Controller(Node):
     def __init__(self):
         super().__init__("controller")
 
-        #create the publisher
-        self.motor_control = self.create_publisher(MotorControl, 'motor_control', 10)
-        self.create_timer(0.001, self.motor_callback)#sends every 0.5 seconds, can be changed
+        # create the publisher
+        # self.motor_publisher = self.create_publisher(MotorControl, 'motor_control', 10)
+        # self.create_timer(0.01, self.motor_callback) # sends every 0.5 seconds, can be changed
     
-        #create the subscribers
-        self.velocity_callback = self.create_subscription(Twist, 'cmd_vel', self.velocity_callback, 10)
+        # create the subscribers
+        self.joy_sub = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
         
-    #function for the subscriber for /cmd_vel
+    # function for the subscriber for /cmd_vel
     def velocity_callback(self, msg):
         
         #extract the linear and angular velocities
-        x = msg.linear.x
-        z = msg.angular.z
+        x = self.joy_scaling(msg.axes[LIN_AXIS])
+        z = TURN_SCALING * self.joy_scaling(msg.axes[ANG_AXIS])
 
-        # Forward (x>0)
-        # Backward (x<0)
-        # Left (z>0)
-        # Right (z<0)
+        self._fl_speed = self.vel_clip(x + z)
+        self._bl_speed = self.vel_clip(x + z)
+        self._fr_speed = self.vel_clip(x - z)
+        self._br_speed = self.vel_clip(x - z)
 
-        if (z > 0):
-            left_speed = z*-1
-            right_speed = z
-        elif (z < 0):
-            left_speed = z
-            right_speed = z*-1
-        else:
-            left_speed = x
-            right_speed = x
-
-
-        self._fl_speed = left_speed
-        self._bl_speed = left_speed
-        self._fr_speed = right_speed
-        self._br_speed = right_speed
-
-    #function for the publisher to /motor_control
-    def motor_callback(self):
+    #f unction for the publisher to /motor_control
+    #def motor_callback(self):
         msg = MotorControl()
         msg.fl = self._fl_speed
         msg.bl = self._bl_speed
         msg.fr = self._fr_speed
         msg.br = self._br_speed
         self.motor_control.publish(msg)
+
+    def joy_scaling(self, x):
+        return 0.0 if abs(x) < DEADZONE else x / (1.0 - DEADZONE)
+    
+    def vel_clip(self, x):
+        return clip(x, -MAX_SPEED, MAX_SPEED)
 
 
 def main(args=None):
