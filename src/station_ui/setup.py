2fd0d32b5c7aff435c663ed81f6d51d900790904
Sriman Selvakumaran <srimans@umd.edu>
2025-03-01 13:44:26 -0500

    launch patch

--- src/station_ui/setup.py
+++ src/station_ui/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joystick_command = station_ui.controller_command:main'
+            'joystick_command = station_ui.joystick_command:main'
         ],
     },
 )
